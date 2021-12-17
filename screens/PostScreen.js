13ec6b7f52af0517e88e840654ebbb47bb774459
Athu5 <84505685+Athu5@users.noreply.github.com>
2021-12-17 23:01:46 +0530

    Add files via upload

---
+++ screens/PostScreen.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+
+import { RFValue } from "react-native-responsive-fontsize";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+import firebase from "firebase";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      light_theme: true,
+      story_id: this.props.story.key,
+      story_data: this.props.story.value,
+      is_liked: false,
+      likes: this.props.story.value.likes
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+}
